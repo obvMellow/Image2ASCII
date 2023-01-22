274365869d0521c4342f59465948ee8f2c8d9118
obvMellow <darkenedMellow@protonmail.com>
2023-01-22 11:14:26 +0300

    Added UI version.

--- Image2ASCII UI/Form1.Designer.cs
+++ Image2ASCII UI/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            path = new TextBox();
+            label1 = new Label();
+            multiplier = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // path
+            // 
+            path.Location = new Point(270, 178);
+            path.Name = "path";
+            path.PlaceholderText = "path/to/image.png";
+            path.Size = new Size(235, 23);
+            path.TabIndex = 0;
+            path.Tag = "input";
+            path.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(270, 160);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Image Path";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // multiplier
+            // 
+            multiplier.Location = new Point(270, 222);
+            multiplier.Name = "multiplier";
+            multiplier.Size = new Size(30, 23);
+            multiplier.TabIndex = 2;
+            multiplier.Tag = "sizeMultiplier";
+            multiplier.Text = "1";
+            multiplier.TextAlign = HorizontalAlignment.Center;
+            multiplier.TextChanged += multiplier_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(270, 204);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Size Multiplier";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(338, 262);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 39);
+            button1.TabIndex = 5;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 32F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(255, 44);
+            label3.Name = "label3";
+            label3.Size = new Size(265, 59);
+            label3.TabIndex = 6;
+            label3.Text = "Image2ASCII";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(768, 465);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(multiplier);
+            Controls.Add(label1);
+            Controls.Add(path);
+            Name = "Image2ASCII";
+            Text = "Image2ASCII";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox path;
+        private Label label1;
+        private TextBox multiplier;
+        private Label label2;
+        private Button button1;
+        private Label label3;
     }
 }
--- Image2ASCII UI/Form1.cs
+++ Image2ASCII UI/Form1.cs
@@ -1,10 +1,93 @@
+using System.Diagnostics;
+using System.Text;
+
 namespace Image2ASCII_UI
 {
     public partial class Form1 : Form
     {
+        private const string brightnessMap = @".'`^,:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists($"output.txt"))
+                File.Delete($"output.txt");
+
+            if (!File.Exists(path.Text))
+            {
+                MessageBox.Show("Cannot find the file specified.");
+                return;
+            }
+
+            float multiplierInt;
+            try
+            {
+                float.Parse(multiplier.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid multiplier.");
+                return;
+            }
+            multiplierInt = float.Parse(multiplier.Text);
+
+            Image image = Image.FromFile(path.Text);
+            Bitmap scaledImage = new(image, new Size((int)(image.Width * multiplierInt), (int)(image.Height * multiplierInt)));
+
+            float strecthFactor = 2f;
+
+            Bitmap stretchedImage = new Bitmap((int)(scaledImage.Width * strecthFactor), (int)(scaledImage.Height * strecthFactor));
+            using (Graphics g = Graphics.FromImage(stretchedImage))
+            {
+                g.ScaleTransform(strecthFactor, 1.0f);
+                g.DrawImage(scaledImage, 0, 0);
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int y = 0; y < stretchedImage.Height / 2; y++)
+            {
+                for (int x = 0; x < stretchedImage.Width; x++)
+                {
+                    int bIndex = (int)(stretchedImage.GetPixel(x, y).GetBrightness() * brightnessMap.Length);
+
+                    if (bIndex < 0)
+                    {
+                        bIndex = 0;
+                    }
+                    else if (bIndex >= brightnessMap.Length)
+                    {
+                        bIndex = brightnessMap.Length - 1;
+                    }
+
+                    stringBuilder.Append(brightnessMap[bIndex]);
+                }
+                stringBuilder.AppendLine();
+            }
+
+            var text = File.CreateText($"output.txt");
+
+            text.Write(stringBuilder.ToString());
+
+            text.Close();
+
+            Process.Start("CMD.exe", $"/C start output.txt");
+        }
+
+        private void multiplier_TextChanged(object sender, EventArgs e)
+        {
         }
     }
 }
